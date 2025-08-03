158ddde61eed994b77a9202f405dff5515dd533c
Ilya Bondarenko <bondarenkoilya2@gmail.com>
2025-08-03 10:24:51 +0300

    bring getDeps to a function, match node modules folders with found deps

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Offline Documentation Viewer for your project Dependencies
 > [!NOTE]
 > This is the NPM equivalent of the Rust package manager’s command: `cargo doc --open`
+
+> [!TIP]
+> I'm not a Node.js developer, so if you notice any serious issues in the code, feel free to point them out.
+
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import fs from "fs";
 
 const projectRoot = path.resolve(process.cwd());
 const packageJSONFilePath = path.join(projectRoot, "package.json");
+const nodeModulesDirectoryPath = path.join(projectRoot, "node_modules");
 
 const readFileAsync = async (path: string) => {
   return new Promise((resolve, reject) =>
@@ -13,13 +14,36 @@ const readFileAsync = async (path: string) => {
   );
 };
 
+const getDependencies = (data: string) => {
+  const { dependencies = {}, devDependencies = {} } = JSON.parse(data);
+
+  const arrayOfDependencies = Object.keys(dependencies);
+  const arrayOfDevDependencies = Object.keys(devDependencies);
+
+  return { arrayOfDependencies, arrayOfDevDependencies };
+};
+
+const matchNodeModulesDependencies = (
+  arrayOfDependencies: string[],
+  arrayOfDevDependencies: string[]
+) => {
+  fs.readdir(nodeModulesDirectoryPath, (error, directories) => {
+    if (error) throw error;
+
+    const matchedDirectories = directories.filter(
+      (directory) =>
+        arrayOfDevDependencies.includes(directory) || arrayOfDependencies.includes(directory)
+    );
+
+    // matchedDirectories.forEach((directory) => {
+    //   const currentDirectoryFilePath = path.join(projectRoot, "node_modules", directory);
+    // });
+  });
+};
+
 readFileAsync(packageJSONFilePath)
-  .then((data: string) => {
-    const { dependencies = {}, devDependencies = {} } = JSON.parse(data);
-
-    const arrayOfDependencies = Object.keys(dependencies);
-    const arrayOfDevDependencies = Object.keys(devDependencies);
-
-    console.log(arrayOfDependencies, arrayOfDevDependencies);
-  })
+  .then(getDependencies)
+  .then(({ arrayOfDependencies, arrayOfDevDependencies }) =>
+    matchNodeModulesDependencies(arrayOfDependencies, arrayOfDevDependencies)
+  )
   .catch((error) => console.log(error.message));
