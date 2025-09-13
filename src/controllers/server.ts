15effa0ec0a5843bfa43a22f0804122a05d3940d
Ilya Bondarenko <bondarenkoilya2@gmail.com>
2025-09-13 21:40:35 +0300

    remove validation because it was useless

--- src/controllers/server.ts
+++ src/controllers/server.ts
@@ -5,45 +5,31 @@ import { promises as fs } from "fs";
 import { getReadmes } from "../models/readme.js";
 import { marked } from "marked";
 
-const nonValidRoutes = ["/favicon.ico", "/.well-known/appspecific/com.chrome.devtools.json"];
-
 const handleRequest = async (request: http.IncomingMessage, response: http.ServerResponse) => {
   try {
-    if (nonValidRoutes.includes(request.url)) {
-      response.writeHead(204);
-      response.end();
-      return;
-    }
-
     const htmlPath = path.join(SRC_PATH, "index.html");
     const htmlFile = await fs.readFile(htmlPath, { encoding: "utf8" });
     const { readmes, dependencies, devDependencies } = await getReadmes();
 
-    const modifyHTML = (): string => {
-      const transferListToHtml = (list: string[]) => [...list].map(createLiElement).join("");
+    const dependencyList = transferListToHtml(dependencies);
+    const devDependencyList = transferListToHtml(devDependencies);
 
-      const currentContent = readmes.find(
-        (dependency) => dependency.directory === request.url.slice(1)
-      );
+    const currentContent = readmes.find(
+      (dependency) => dependency.directory === request.url.slice(1)
+    );
 
-      const dependencyList = transferListToHtml(dependencies);
-      const devDependencyList = transferListToHtml(devDependencies);
-      // TODO: Attention to this conditions. Refactor maybe
-      const content =
-        currentContent && currentContent.content
-          ? marked.parse(currentContent.content)
-          : marked.parse(readmes[0].content);
+    const content =
+      currentContent && currentContent.content
+        ? marked.parse(currentContent.content)
+        : marked.parse(readmes[0].content);
 
-      const modifiedHtml = htmlFile
-        .replace("{{content}}", content as string)
-        .replace("{{dependencyList}}", dependencyList)
-        .replace("{{devDependencyList}}", devDependencyList);
-
-      return modifiedHtml;
-    };
+    const modifiedHtml = htmlFile
+      .replace("{{content}}", content as string)
+      .replace("{{dependencyList}}", dependencyList)
+      .replace("{{devDependencyList}}", devDependencyList);
 
     response.writeHead(200, { "Content-Type": "text/html" });
-    response.end(modifyHTML());
+    response.end(modifiedHtml);
   } catch (error) {
     response.writeHead(500);
     response.end(`${error}`);
@@ -55,6 +41,7 @@ export const createServer = (): http.Server => http.createServer(handleRequest);
 export const startServer = (server: http.Server): http.Server =>
   server.listen(PORT, () => console.log(`Server started on http://localhost:${PORT}`));
 
+// TODO: Bring those two to a separate file and return already modified deps
 function createLiElement(text: string): string {
   // TODO: Fix the bug with URLs that has been changed (from / to -, etc)
   const url = text.split("/").join("-");
@@ -62,4 +49,8 @@ function createLiElement(text: string): string {
   return `<li class="navbar__list-item">
     <a class="navbar__list-link" href=${url}>${text}</a>
   </li>`;
+}
+
+function transferListToHtml(list: string[]) {
+  return [...list].map(createLiElement).join("");
 }
