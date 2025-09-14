f40a8490a67d159beb5cab8cd4eca8a4116635d6
Ilya Bondarenko <bondarenkoilya2@gmail.com>
2025-09-14 08:52:30 +0300

    bring http codes and content type to the constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -5,3 +5,9 @@ export const SRC_PATH = path.resolve(PROJECT_ROOT, "src");
 export const PACKAGE_JSON_PATH = path.join(PROJECT_ROOT, "package.json");
 export const NODE_MODULES_PATH = path.join(PROJECT_ROOT, "node_modules");
 export const PORT = 5123;
+
+export const CONTENT_TYPE_HTML = "text/html";
+export const HTTP_STATUS = {
+  OK: 200,
+  INTERNAL_SERVER_ERROR: 500
+};
--- src/controllers/server.ts
+++ src/controllers/server.ts
@@ -1,10 +1,11 @@
 import http from "http";
-import { PORT, SRC_PATH } from "../constants/index.js";
+import { HTTP_STATUS, PORT, SRC_PATH } from "../constants/index.js";
 import path from "path";
 import { promises as fs } from "fs";
 import { getReadmes } from "../models/readme.js";
 import { marked } from "marked";
 import { transferListToHtml } from "../utils/handleDependencies.js";
+import { CONTENT_TYPE_HTML } from "../constants/index.js";
 
 const handleRequest = async (request: http.IncomingMessage, response: http.ServerResponse) => {
   try {
@@ -15,25 +16,21 @@ const handleRequest = async (request: http.IncomingMessage, response: http.Serve
     const dependencyList = transferListToHtml(dependencies);
     const devDependencyList = transferListToHtml(devDependencies);
 
-    const currentContent = readmes.find(
+    // TODO: Fix the bug with URLs that has been changed (from / to -, etc)
+    const selectedContent = readmes.find(
       (dependency) => dependency.directory === request.url.slice(1)
     );
-
-    // TODO: Fix the bug with URLs that has been changed (from / to -, etc)
-    const content =
-      currentContent && currentContent.content
-        ? marked.parse(currentContent.content)
-        : marked.parse(readmes[0].content);
+    const renderedContent = selectedContent?.content || readmes[0].content;
 
     const modifiedHtml = htmlFile
-      .replace("{{content}}", content as string)
+      .replace("{{content}}", marked.parse(renderedContent) as string)
       .replace("{{dependencyList}}", dependencyList)
       .replace("{{devDependencyList}}", devDependencyList);
 
-    response.writeHead(200, { "Content-Type": "text/html" });
+    response.writeHead(HTTP_STATUS.OK, { "Content-Type": CONTENT_TYPE_HTML });
     response.end(modifiedHtml);
   } catch (error) {
-    response.writeHead(500);
+    response.writeHead(HTTP_STATUS.INTERNAL_SERVER_ERROR);
     response.end(`${error}`);
   }
 };
